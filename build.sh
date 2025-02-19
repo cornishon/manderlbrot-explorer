2a638210130f9cc46fa65647e752de1cc64b48a5
Adam Zadrożny <zadroznyadam@protonmail.com>
2025-02-19 11:10:10 +0100

    Fix the colors

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@ ODIN=~/opt/Odin/odin
 
 set -xe
 
+MAX_ITER=${MAX_ITER:-400}
 CELL_SIZE=${CELL_SIZE:-8}
 shader=mandelbrot.comp
-glslc $shader -o $shader.spv -DCELL_SIZE=$CELL_SIZE
+glslc $shader -o $shader.spv -DCELL_SIZE=$CELL_SIZE -DMAX_ITER=$MAX_ITER
 
 case "$1" in
     "check")
